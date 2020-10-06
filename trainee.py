8bb60ffba69664a291e47af45358426c6a23be11
Vladyslav Mykytenko <mykytenkovladislav@gmail.com>
2020-10-06 21:55:46 +0300

    rewrite to better solution #2

--- trainee.py
+++ trainee.py
@@ -1,15 +1,14 @@
 string: str = 'If I look back I am lost'
 
 
-def count_char(string_come: str, char_rec: str):
-    counter: int = 0
-    match_count: int = 0
-    while counter < len(string_come):
-        temp_char: str = string_come[counter]
-        if temp_char == char_rec:
-            match_count += 1
-        counter += 1
-    return match_count
+def count_char(string, char):
+    index = len(string) - 1
+    result = 0
+    while index >= 0:
+        if string[index] == char:
+            result += 1
+        index -= 1
+    return result
 
 
-print(count_char(string, 't'))
+print(count_char(string, 'f'))
