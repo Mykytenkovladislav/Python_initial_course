56c4cc996b0f780e88cb71a5265d0bf796e18486
mykytenkovladislav <mykytenkovladislav@gmail.com>
2020-11-01 12:01:23 +0200

    practice from lesson 13

--- Additional files/Lesson 13/practice.py
+++ Additional files/Lesson 13/practice.py
@@ -19,3 +19,30 @@ def write(what_to_write):
 
 copied_text = read()
 write(copied_text)
+
+# OOP
+class Road:
+    def __init__(self, quality, material=None):
+        self.quality = quality
+        if quality > 60:
+            self.material = material
+
+
+class Car:
+    def __init__(self, max_speed=0, weight=0, model='Lada'):
+        self.weight = weight
+        self.max_speed = max_speed
+        self.name = model
+
+    def drive(self, current_speed=0, start_route='start_point', end_route='end_route', road: Road = None):
+        if self.max_speed < current_speed:
+            current_speed = self.max_speed
+            print(current_speed)
+        else:
+            print(f'Drive from {start_route} to {end_route} with speed {current_speed}')
+
+
+mercedes = Car(30, 200, 'Mercedes', 'Parapapam')
+road_a = (67, 'asphalt')
+tesla_x = Car(max_speed=250, weight=2000, model='Tesla X')
+tesla_x.drive(current_speed=300, road=road_a)
--- vladyslav.py
+++ vladyslav.py
@@ -1,19 +1,25 @@
-def zip_func(*args):
-    iterators: iter = [iter(it) for it in args]
-    empty_object = object()
-    while iterators:
-        result: list = []
-        for it in iterators:
-            elem = next(it, empty_object)
-            if elem is empty_object:
-                return
-            result.append(elem)
-        yield tuple(result)
+class Road:
+    def __init__(self, quality, material=None):
+        self.quality = quality
+        if quality > 60:
+            self.material = material
 
 
-a = [1, 2, 3]
-b = [1, 2, 3]
-c = [1, 2, 3]
+class Car:
+    def __init__(self, max_speed=0, weight=0, model='Lada'):
+        self.weight = weight
+        self.max_speed = max_speed
+        self.name = model
 
-for item in (zip_func(a, b, c)):
-    print(item)
+    def drive(self, current_speed=0, start_route='start_point', end_route='end_route', road: Road = None):
+        if self.max_speed < current_speed:
+            current_speed = self.max_speed
+            print(current_speed)
+        else:
+            print(f'Drive from {start_route} to {end_route} with speed {current_speed}')
+
+
+mercedes = Car(30, 200, 'Mercedes', 'Parapapam')
+road_a = (67, 'asphalt')
+tesla_x = Car(max_speed=250, weight=2000, model='Tesla X')
+tesla_x.drive(current_speed=300, road=road_a)
